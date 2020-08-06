ad2d20a21790ce52d70133e97bc223651d2434d7
Jean Osco <willjean29@gmail.com>
2020-08-06 12:40:18 -0500

    docs agregado

---
+++ docs/precache-manifest.91e5a2b5ba51af28645ea20bc4a59b9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c8a698f209737e76aea7570ae777a0e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3fa1cd4440bfdea0677",
+    "url": "/static/css/main.fb8c18af.chunk.css"
+  },
+  {
+    "revision": "6b1578778fd38a87f0cb",
+    "url": "/static/js/2.a685c4d8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a685c4d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3fa1cd4440bfdea0677",
+    "url": "/static/js/main.b6ad1373.chunk.js"
+  },
+  {
+    "revision": "c8429c3d75b971d53245",
+    "url": "/static/js/runtime-main.7b6f9e01.js"
+  }
+]);
